9ae1be8f3dabfad49c9bd29c3e1bc719ad9a9330
Jonas Otten <jonadev95@posteo.org>
2021-05-25 00:25:57 +0100

    test more

--- test/clojureflare/core_test.cljs
+++ test/clojureflare/core_test.cljs
@@ -2,12 +2,20 @@
   (:require [cljs.test]
             [clojureflare.core]))
 
-; simple route 
+                                        ; validate route schema
 (cljs.test/deftest simple-route
   (let [r (clojureflare.core/route "GET" "/v1" "hello world")]
     (cljs.test/is (= r {:method "GET" :path "/v1" :handler "hello world"}))))
 
+                                        ; check if URL can be extracted
+(cljs.test/deftest extract-url
+  (let [ep (clojureflare.core/extract-path (js/URL. "http://localhost/api/v1/test"))]
+    (cljs.test/is (= ep "/api/v1/test"))))
 
+                                        ; convert request from JS object
+(cljs.test/deftest convert-request
+  (let [cr (clojureflare.core/convert-request #js {:url "http://localhost/api/v1/test"})]
+    (cljs.test/is (= cr {:path "/api/v1/test"}))))
 
 
 (cljs.test/run-tests)
