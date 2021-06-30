84516a4602f0e7200c38cac6ecc27dfe05396036
Jonas Otten <jonadev95@posteo.org>
2021-06-30 08:10:15 +0100

    fix test path

--- example-worker/project.clj
+++ example-worker/project.clj
@@ -2,10 +2,10 @@
   :description "Example Clojure Cloudflare worker"
   :plugins [[lein-cljsbuild "1.1.8"]]
   :dependencies [[org.clojure/clojure "1.10.3"]
-                 [org.clojure/clojurescript "1.10.339"]
-                 [com.github.sauercrowd/clojureflare "0.0.1"]]
+                 [org.clojure/clojurescript "1.10.339"]]
   :source-paths ["src"]
   :hooks [leiningen.cljsbuild]
+  :resource-paths ["../target/clojureflare-0.1.0-SNAPSHOT.jar"]
   :cljsbuild {
     :builds {:production
                {:source-paths ["src"]
